00b75367fd78d7dda8a8dd8ca7dd3cd0d32f141a
Michał Lipek <michal@lipek.net>
2013-01-06 02:20:30 +0100

    HTML builder class

---
+++ src/Nautilus/Form/Html/Tag.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Nautilus\Form\Html;
+
+class Tag
+{
+
+    private $tag;
+
+    private $attributes = array();
+
+    private $html;
+
+    public function __construct($tag)
+    {
+        $this->setTag($tag);
+    }
+
+    public function setTag($tag)
+    {
+        if (!preg_match('/^[a-z][a-z0-9\-]*$/', $tag)) {
+            throw new \RuntimeException("Incorrect tag name \"{$tag}\"");
+        }
+        $this->tag = $tag;
+        return $this;
+    }
+
+    public function getTag()
+    {
+        return $this->tag;
+    }
+
+    public function setAttribute($attribute, $value)
+    {
+        if (!preg_match('/^[a-z_\-:][-a-z0-9_:\-]*$/', $attribute)) {
+            throw new \RuntimeException("Incorrect attribute name \"{$attribute}{\"");
+        }
+        $this->attributes[$attribute] = $value;
+        return $this;
+    }
+
+    public function getAttribute($attribute)
+    {
+        if (!isset($this->attributes[$attribute])) {
+            return '';
+        }
+        return $this->attributes[$attribute];
+    }
+
+    public function removeAttribute($attribute)
+    {
+        if (isset($this->attributes[$attribute])) {
+            unset($this->attributes[$attribute]);
+        }
+        return $this;
+    }
+
+    public function setHtml($html)
+    {
+        $this->html = $html;
+        return $this;
+    }
+
+    public function getHtml()
+    {
+        return $this->html;
+    }
+
+    public function render()
+    {
+        $result = <<<HTML
+<{$this->getTag()} {$this->getAttributesString()}>{$this->getHtml()}</{$this->getTag()}>
+HTML;
+        return $result;
+    }
+
+    public function __toString(){
+        return $this->render();
+    }
+
+    private function getAttributesString()
+    {
+        $result = '';
+        foreach ($this->attributes as $k => $v) {
+            $v = $this->escapeValue($v);
+            $result .= "$k=\"$v\" ";
+        }
+        return trim($result);
+    }
+
+    private function escapeValue($value)
+    {
+        return htmlentities($value, ENT_COMPAT, 'UTF-8');
+    }
+
+}
---
+++ tests/src/Nautilus/Form/Html/TagTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Nautilus\Form\Html\Tests;
+
+use Nautilus\Form\Html\Tag;
+
+class TagTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testAttributes()
+    {
+        $h1 = new Tag('h1');
+        $h1->setAttribute('class', 'big-header');
+        $this->assertEquals('big-header', $h1->getAttribute('class'));
+
+        $h1->removeAttribute('class');
+        $this->assertEquals('', $h1->getAttribute('class'));
+    }
+
+    public function testHtml()
+    {
+        $div = new Tag('div');
+        $this->assertEquals('', $div->getHtml());
+
+        $html = '<p class="x">test &quot;</p>';
+        $div->setHtml($html);
+        $this->assertEquals($html, $div->getHtml());
+    }
+
+    public function testFluentInterface()
+    {
+        $ul = new Tag('ul');
+        $this->assertEquals($ul, $ul->setAttribute('class', 'middle')->setHtml('<li>point 1</li>'));
+        $this->assertEquals($ul, $ul->setHtml('<li>point 1</li>')->setAttribute('class', 'middle'));
+    }
+
+    public function testRender()
+    {
+        $div = new Tag('div');
+        $this->assertXmlStringEqualsXmlString('<div></div>', $div->render());
+        $this->assertXmlStringEqualsXmlString('<div></div>', (string)$div);
+
+        $div->setAttribute('data-myvalue', '15')->setHtml('test');
+        $this->assertXmlStringEqualsXmlString('<div data-myvalue="15">test</div>', $div->render());
+
+        $div->setAttribute('data-tag', '"<div>"');
+        $this->assertXmlStringEqualsXmlString('<div data-myvalue="15" data-tag="&quot;&lt;div&gt;&quot;">test</div>', $div->render());
+    }
+
+    public function testInvalidAttributeNameWithSpace()
+    {
+        $this->setExpectedException('\RuntimeException');
+        $div = new Tag('div');
+        $div->setAttribute('invalid attribute', 1);
+    }
+
+    public function testInvalidAttributeNameDigitFirst() {
+        $this->setExpectedException('\RuntimeException');
+        $div = new Tag('div');
+        $div->setAttribute('123qwe', "456rty");
+    }
+
+    public function testInvalidTagName()
+    {
+        $this->setExpectedException('\RuntimeException');
+        $div = new Tag('<div>');
+    }
+
+    public function testInvalidTagNameWithBraces()
+    {
+        $this->setExpectedException('\RuntimeException');
+        $div = new Tag('<div>');
+    }
+
+    public function testInvalidTagNameWithSpace()
+    {
+        $this->setExpectedException('\RuntimeException');
+        $div = new Tag('input checked');
+    }
+}
